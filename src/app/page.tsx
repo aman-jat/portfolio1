165f45ea7e973d363e495c389a4b5a3f10ee215d
Aman Jat <amanjat261@gmail.com>
2024-06-02 16:32:46 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,36 @@
 export default function App() {
-  return <div className='bg-primary'>Hello</div>;
+  const navItems = [
+    {
+      name: 'Home',
+      link: '/',
+    },
+    {
+      name: 'About',
+      link: '/about',
+    },
+    {
+      name: 'Contact',
+      link: '/contact',
+    },
+    {
+      name: 'Services',
+      link: '/services',
+    },
+  ];
+  return (
+    <div className='bg-dark-gray-gradient flex items-center justify-center w-full '>
+      <div className='flex w-full items-center max-w-[800px] justify-between px-4'>
+        <div className='text-white font-bold text-2xl'>Logo</div>
+        <div>
+          <ul className='flex text-white gap-4'>
+            {navItems.map(item => (
+              <li key={item.name} className=''>
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'dark-gray-gradient': 'radial-gradient(circle,#3a3a3a,#000000)', // Add this line
       },
     },
   },
