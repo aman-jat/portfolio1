469eeef0c53c62d8c4fde6b35e5ce7b5f18c5289
Aman Jat <amanjat261@gmail.com>
2024-06-17 08:21:14 +0530

    Updates

---
+++ src/app/data.ts
@@ -0,0 +1,293 @@
+import html from '../images/html.png';
+import css from '../images/css.png';
+import react from '../images/react.png';
+import node from '../images/node.png';
+import express from '../images/express.png';
+import mongodb from '../images/mongodb.png';
+import postgres from '../images/postgres.png';
+import next from '../images/next.png';
+import tailwind from '../images/tailwind.png';
+import sass from '../images/sass.png';
+import bootstrap from '../images/bootstrap.png';
+import mui from '../images/MUI.png';
+import angular from '../images/angular.png';
+import c from '../images/c.png';
+import cplusplus from '../images/cplusplus.png';
+import python from '../images/python.png';
+import formik from '../images/formik.png';
+import java from '../images/java.png';
+import postman from '../images/postman.png';
+import mysql from '../images/mysql.png';
+import solidity from '../images/solidity.png';
+import fastapi from '../images/fastapi.png';
+import redux from '../images/redux.png';
+import rxjs from '../images/rxjx.png';
+import sequelize from '../images/sequelize.png';
+import laravel from '../images/laravel.png';
+import firebase from '../images/firebase.png';
+import typescript from '../images/ts.png';
+import javascript from '../images/js.png';
+import php from '../images/php.png';
+import jest from '../images/jest.png';
+import vitest from '../images/vitest.png';
+import mocha from '../images/mocha.png';
+import chai from '../images/chai.png';
+import reactTestingLibrary from '../images/reactTestingLibrary.png';
+import aws from '../images/aws.png';
+import azure from '../images/azure.png';
+import gcp from '../images/gcp.png';
+
+export const navItems = [
+  {
+    id: 1,
+    name: 'Projects',
+    to: '/projects',
+  },
+  {
+    id: 2,
+    name: 'Services',
+    to: '/services',
+  },
+  {
+    id: 3,
+
+    name: 'Feedback',
+    to: '/feedback',
+  },
+  {
+    id: 4,
+    name: 'Contacts',
+    to: '/contacts',
+  },
+];
+
+export const technologiesGroup = [
+  {
+    title: 'Frontend Frameworks',
+    technologies: [
+      {
+        name: 'NextJS',
+        icon: next,
+      },
+      {
+        name: 'React',
+        icon: react,
+      },
+      {
+        name: 'React Native',
+        icon: react,
+      },
+      {
+        name: 'Redux',
+        icon: redux,
+      },
+      {
+        name: 'Formik',
+        icon: formik,
+      },
+      {
+        name: 'Angular',
+        icon: angular,
+      },
+      {
+        name: 'RX/JX',
+        icon: rxjs,
+      },
+      {
+        name: 'Sass',
+        icon: sass,
+      },
+      {
+        name: 'Bootstrap',
+        icon: bootstrap,
+      },
+      {
+        name: 'Material UI',
+        icon: mui,
+      },
+      {
+        name: 'Tailwind',
+        icon: tailwind,
+      },
+    ],
+  },
+  {
+    title: 'Programming Languages',
+    technologies: [
+      {
+        name: 'Python',
+        icon: python,
+      },
+      {
+        name: 'Typescript',
+        icon: typescript,
+      },
+      {
+        name: 'Javascript',
+        icon: javascript,
+      },
+      {
+        name: 'PHP',
+        icon: php,
+      },
+      {
+        name: 'Solidity',
+        icon: solidity,
+      },
+      {
+        name: 'C',
+        icon: c,
+      },
+      {
+        name: 'C++',
+        icon: cplusplus,
+      },
+      {
+        name: 'Java',
+        icon: java,
+      },
+
+      {
+        name: 'HTML',
+        icon: html,
+      },
+
+      {
+        name: 'CSS',
+        icon: css,
+      },
+    ],
+  },
+  {
+    title: 'Backend Frameworks',
+    technologies: [
+      {
+        name: 'FastAPI',
+        icon: fastapi,
+      },
+      {
+        name: 'Node JS',
+        icon: node,
+      },
+      {
+        name: 'Express.js',
+        icon: express,
+      },
+      {
+        name: 'Sequelize',
+        icon: sequelize,
+      },
+      {
+        name: 'Laravel',
+        icon: laravel,
+      },
+    ],
+  },
+  {
+    title: 'Testing Tools',
+    technologies: [
+      {
+        name: 'Jest',
+        icon: jest,
+      },
+      {
+        name: 'Vitest',
+        icon: vitest,
+      },
+      {
+        name: 'Mocha',
+        icon: mocha,
+      },
+      {
+        name: 'Chai',
+        icon: chai,
+      },
+      {
+        name: 'React Testing Library',
+        icon: reactTestingLibrary,
+      },
+      {
+        name: 'Postman',
+        icon: postman,
+      },
+    ],
+  },
+
+  {
+    title: 'Databases',
+    technologies: [
+      {
+        name: 'MongoDB',
+        icon: mongodb,
+      },
+      {
+        name: 'Postgres',
+        icon: postgres,
+      },
+      {
+        name: 'MySQL',
+        icon: mysql,
+      },
+      {
+        name: 'Firebase',
+        icon: firebase,
+      },
+    ],
+  },
+  {
+    title: 'Future interest',
+    technologies: [
+      {
+        name: 'AWS',
+        icon: aws,
+      },
+      {
+        name: 'Azure',
+        icon: azure,
+      },
+      {
+        name: 'GCP',
+        icon: gcp,
+      },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    name: 'Personal Website',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget urna auctor, aliquet nunc id, tincidunt libero.',
+    viewProject: 'View Project',
+    download: 'Download',
+    link: 'http://localhost:3000/',
+  },
+  {
+    id: 2,
+    name: 'Catalyst',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget urna auctor, aliquet nunc id, tincidunt libero.',
+    viewProject: 'View Project',
+    download: 'Download',
+    link: 'https://dev-app.catalystai.work/',
+  },
+  {
+    id: 3,
+    name: 'Feenix',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget urna auctor, aliquet nunc id, tincidunt libero.',
+    viewProject: 'View Project',
+    download: 'Download',
+    link: 'https://www.feenix.ai/',
+  },
+  {
+    id: 4,
+    name: 'Exxat',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget urna auctor, aliquet nunc id, tincidunt libero.',
+    viewProject: 'View Project',
+    download: 'Download',
+    link: 'https://www.exxat.com',
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,169 @@
+'use client';
+
+import {
+  faTwitter,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import { faDownload, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
+import Image from 'next/image';
+import aman from './aman.jpg';
+import { navItems, projects, technologiesGroup } from './data';
+import Microlink from '@microlink/react';
+
 export default function App() {
-  const navItems = [
+  const socialMediaLink = [
     {
-      name: 'Home',
-      link: '/',
+      id: 1,
+      name: 'Twitter',
+      to: 'https://www.facebook.com',
+      icon: <FontAwesomeIcon icon={faTwitter} size='1x' />,
     },
     {
-      name: 'About',
-      link: '/about',
+      id: 2,
+      name: 'LinkedIN',
+      to: 'https://www.twitter.com',
+      icon: <FontAwesomeIcon icon={faLinkedin} size='1x' />,
     },
     {
-      name: 'Contact',
-      link: '/contact',
-    },
-    {
-      name: 'Services',
-      link: '/services',
+      id: 3,
+      name: 'Github',
+      to: 'https://www.instagram.com',
+      icon: <FontAwesomeIcon icon={faGithub} size='1x' />,
     },
   ];
+
   return (
-    <div className='bg-dark-gray-gradient flex items-center justify-center w-full '>
-      <div className='flex w-full items-center max-w-[800px] justify-between px-4'>
-        <div className='text-white font-bold text-2xl'>Logo</div>
-        <div>
-          <ul className='flex text-white gap-4'>
-            {navItems.map(item => (
-              <li key={item.name} className=''>
-                {item.name}
-              </li>
+    <>
+      {/* body */}
+      <div className='  bg-dark-gray-gradient py-8 px-8 flex flex-col items-center justify-center w-full '>
+        {/*  */}
+        {/* header */}
+        <div className='max-w-[1400px] flex w-full items-center justify-between px-4'>
+          {/* left hand side */}
+          <div className='text-white flex gap-8 flex-row items-center'>
+            <p className='text-2xl'>
+              <b> Aman </b>Jat
+            </p>
+            <ul className='flex text-white gap-4'>
+              {navItems.map(item => (
+                <li key={item.name} className=''>
+                  <a href={item.to} className='underline-animation '>
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* right hand side - Social Media Icons */}
+          <div className='flex text-white gap-6'>
+            {socialMediaLink.map(item => (
+              <a
+                href={item.to}
+                className='flex  flex-row  items-center justify-between gap-2.5'
+                key={item.name}>
+                {item.icon}
+                <p className='text-neutral-400  font-medium  text-sm underline-animation'>
+                  {item.name}
+                </p>
+              </a>
             ))}
-          </ul>
+            <div className='flex text-primary cursor-pointer items-center justify-center p-4 rounded-full bg-neutral-600 bg-opacity-30 hover:bg-opacity-70  transition duration-500 ease-in-out'>
+              <FontAwesomeIcon icon={faEnvelope} size='2x' />
+            </div>
+          </div>
+        </div>
+
+        {/*  */}
+        {/* intro */}
+        <div className='max-w-[1400px] flex flex-row gap-8'>
+          <div className='flex flex-col items-start justify-center text-white'>
+            <h1 className='text-7xl '>
+              Hello, I'm <span className='font-bold'>Aman Jat</span>{' '}
+            </h1>
+            <p className='text-lg font-medium'>
+              I am a Full Stack Developer, who loves to code and build things.
+            </p>
+            <div className='flex flex-row gap-4'>
+              <div className='bg-primary py-2 px-8 rounded-lg shadow-lg my-4'>
+                <p className='text-lg font-medium'>Full Stack Developer</p>
+              </div>
+              <div className='cursor-pointer border-primary border-2 text-primary py-2 px-8 rounded-lg shadow-lg my-4 flex flex-row items-center'>
+                <p className='text-lg font-medium pr-4'>Download CV</p>
+                <FontAwesomeIcon icon={faDownload} size='1x' />
+              </div>
+            </div>
+          </div>
+          <div className='py-10'>
+            <div
+              className={`w-[600px]  rounded-full overflow-hidden border-4 border-white border-opacity-50 shadow-lg`}>
+              <Image
+                src={aman} // replace with the correct path
+                alt='Profile'
+                className='grayscale'
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* {PROJECTS} */}
+        <div className='max-w-[1400px] px-8 w-full flex flex-col items-start'>
+          {/* <h1 className='h2 text-white  text-4xl'>Projects</h1> */}
+          <div className='grid w-full grid-flow-row  md:grid-cols-1 lg:grid-cols-2 py-4  gap-4'>
+            {technologiesGroup.map(group => {
+              return (
+                <div
+                  key={group.title}
+                  className='p-8  flex flex-col bg-secondary-200 bg-opacity-5 rounded-lg gap-4 items-start justify-start   '>
+                  <h1 className='text-3xl text-white'>{group.title}</h1>
+                  <div className='grid grid-flow-row  md:grid-cols-4 lg:grid-cols-4 py-4  gap-4'>
+                    {group.technologies.map(tech => {
+                      return (
+                        <div
+                          key={tech.name}
+                          className='p-4 w-[120px] h-[160px] flex justify-end flex-col bg-secondary-200 bg-opacity-5 rounded-lg gap-4 items-start    '>
+                          <Image
+                            src={tech.icon} // replace with the correct path
+                            alt='Profile'
+                            width={50}
+                            height={50}
+                          />
+                          <h1 className='text-base text-white'>{tech.name}</h1>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className='grid grid-flow-row  md:grid-cols-1 lg:grid-cols-2 py-4  gap-4'>
+            {projects.map(project => {
+              return (
+                <div
+                  key={project.id}
+                  className='p-8 flex flex-col bg-secondary-200 bg-opacity-5 rounded-lg gap-4 items-start justify-center border-2 shadow-lg border-black border-opacity-20 '>
+                  <h1 className='text-3xl text-white'>{project.name}</h1>
+                  <Microlink url={project.link} />
+                  <p className='text-lg text-neutral-400'>
+                    {project.description}
+                  </p>
+                  <div className='flex flex-row gap-4'>
+                    <div className='bg-primary py-2 px-8 rounded-lg shadow-lg '>
+                      <p className='text-lg font-medium'>View on Github</p>
+                    </div>
+                    <div className='cursor-pointer border-primary border-2 text-primary py-2 px-8 rounded-lg shadow-lg  flex flex-row items-center'>
+                      <p className='text-lg font-medium'>VISIT</p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
